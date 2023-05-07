698f0d84ed19f4cd37ae8045b906777b75d982f4
Whitneyas <whitneyserwaa@gmail.com>
2023-05-07 14:09:53 +0200

    editted

--- script.js
+++ script.js
@@ -1,68 +1,148 @@
-//Variables 
+// const radios = document.querySelectorAll('.grade-scale input[type="radio"]');
+// const output = document.getElementById('output');
 
-// // Get the form and input fields
-// const form = document.querySelector('form');
-// const nameInput = document.querySelector('#name');
-// const emailInput = document.querySelector('#email');
-// const studentNumberInput = document.querySelector('#student-number');
-
-// //load input values on submit
-// nameInput.value = localStorage.getItem('name');
-// emailInput.value = localStorage.getItem('email');
-// studentNumberInput.value = localStorage.getItem('student-number');
-
-// //save input values on submit 
-// form.addeventListner('submit' , (event) => {
-//     event.preventDefault();
-//     localStorage.setItem ('name' , nameInput.value);
-//     localStorage.setItem ('email',emailInput.value);
-//     localStorage.setItem ('student-number', studentNumberInput.value);
-//     alert('input values saved')
-// })
+// radios.forEach(radio => {
+//   radio.addEventListener('change', (event) => {
+//     output.innerHTML = `You selected option ${event.target.value}.`;
+//   });
+// });
 
 
 
-const studentInfo = document.getElementById("personal-info");
+console.log('hello')
+//Variabele
+const textAreas = document.querySelectorAll('textarea');
+const formInputs = document.querySelectorAll('input[type="text"] , input[type="email"]');
+let formRadios = document.querySelectorAll('input[type="radio"]');
+const testRadioButtons = document.querySelectorAll('input[type="radio"]')
+const email = document.getElementById("email");
+// const textarea = document.querySelector('textarea');
+// var messageDisplayed = false;
 
-studentInfo.addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent the form from submitting
-  
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const studentIdInput = document.getElementById("student-id");
- 
-  
-  // Check if name input is not empty
-  if (nameInput.value.trim() === "") {
-    alert("Please enter your name");
-    return;
-  }
-  
-  // Check if email input is a valid email format
-  if (!isValidEmail(emailInput.value)) {
-    alert("Please enter a valid email address");
-    return;
-  }
-  
-  // Check if student ID input contains 9 digits
-  if (!isValidStudentId(studentIdInput.value)) {
-    alert("Please enter a student ID containing 9 digits");
-    return;
-  }
-  
-  // If all validation passes, submit the form
-  studentInfo.submit();
+
+
+//---------------------------Show the value that the user chose
+
+testRadioButtons.forEach(button => {
+  button.addEventListener('change', () => {
+    const getRadioName = button.name;
+    const getRadioValue = button.value;
+
+    if (button.checked) {
+      switch (getRadioName) {
+        case 'grade1':
+          document.querySelector('.results1').innerHTML = `You've graded this subject on a scale of  ${getRadioValue}.`
+          break;
+        case 'grade2':
+          document.querySelector('.results2').innerHTML = `You've graded this subject on a scale of ${getRadioValue}.`
+          break;
+        case 'grade3':
+          document.querySelector('.results3').innerHTML = `You've graded this subject on a scale of ${getRadioValue}.`
+          break;
+        case 'Css-grade1':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'Css-grade2':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'Css-grade3':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+        case 'pwa-lesstof-beoordeling':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'pwa-uitleg-beoordeling':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'pwa-eigen-inzicht':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+        case 'brotech-lesstof-beoordeling':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'brotech-uitleg-beoordeling':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'brotech-eigen-inzicht':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+        case 'realtimeweb-lesstof-beoordeling':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'realtimeweb-uitleg-beoordeling':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'realtimeweb-eigen-inzicht':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+        case 'human-centred-design-lesstof-beoordeling':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'human-centred-design-uitleg-beoordeling':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'human-centred-design-eigen-inzicht':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+        case 'meesterproef-lesstof-beoordeling':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'meesterproef-uitleg-beoordeling':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'meesterproef-eigen-inzicht':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+        case 'weekly-nerd-lesstof-beoordeling':
+          document.querySelector('.results1').innerHTML = `Je geeft de lesstof een ${getRadioValue}.`
+          break;
+        case 'weekly-nerd-uitleg-beoordeling':
+          document.querySelector('.results2').innerHTML = `Je geeft de uitleg een ${getRadioValue}.`
+          break;
+        case 'weekly-nerd-eigen-inzicht':
+          document.querySelector('.results3').innerHTML = `Je geeft je eigen inzicht een ${getRadioValue}.`
+          break;
+
+
+        default:
+          break;
+      }
+    }
+  });
 });
 
-function isValidEmail(email) {
-  // Regular expression to check email format
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
 
-function isValidStudentId(studentId) {
-  // Regular expression to check if student ID contains 9 digits
-  const studentIdRegex = /^[0-9]{9}$/;
-  return studentIdRegex.test(studentId);
-}
+// Giving feedback to the user 
 
+textAreaValidation()
+
+function textAreaValidation() {
+  const textAreaInputs = document.querySelectorAll('textarea');
+
+
+  textAreaInputs.forEach(area => {
+    area.addEventListener('input', () => {
+      let textAreaValues = area.value;
+      let span = document.createElement('span');
+      area.parentNode.insertBefore(span, area.nextSibling);
+
+
+      if (textAreaValues !== '') {
+        if (!area.dataset.typed) {
+          span.textContent = 'Well done';
+          span.classList.add("valid-feedback")
+
+        }
+        area.dataset.typed = 'true';
+        area.classList.add('valid-data');
+
+      } else {
+        span.textContent = ''
+        area.dataset.typed = '';
+        area.classList.remove('valid-data')
+      }
+    })
+
+
+  })
+}
